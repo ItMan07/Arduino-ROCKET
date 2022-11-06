ed57e5c36c1efa96ece44f75535573786888966b
ItMan <kurbanovarslan228@gmail.com>
2022-11-06 20:16:43 +0500

    update servodrive

--- src/modules/servodrive.h
+++ src/modules/servodrive.h
@@ -1,26 +1,35 @@
-// #include "config.h"
+#include "ServoSmooth.h"
 
-Servo servo1;
+int max_angle = 180;
+
+ServoSmooth servo;
+// ServoSmooth servo(max_angle);
+
+void setup_servo()
+{
+    servo.attach(PIN_SERVO);
+    servo.setSpeed(180);
+    servo.setAccel(0.5);
+}
 
 void servo_write(byte angle)
 {
-  servo1.write(angle);
-  // delay(delay_time);
+    servo.setTargetDeg(angle);
 
-  serialSend(4, angle); // отправляем на комп
+    serialSend(4, angle);
 }
 
 byte read_potent()
 {
-  static int val = 0;
-  static int last_val = 0;
-  
-  val = analogRead(A5);
-  val = map(val, 0, 1023, 0, 179);
+    static int val = 0;
+    static int last_val = 0;
 
-  if ((val > last_val && val - last_val >= 2) || (val < last_val && last_val - val >= 2))
-  {
-    last_val = val;
-    return val;
-  }
+    val = analogRead(A5);
+    val = map(val, 0, 1023, 0, 180);
+
+    if ((val > last_val && val - last_val >= 2) || (val < last_val && last_val - val >= 2))
+    {
+        last_val = val;
+        return val;
+    }
 }
